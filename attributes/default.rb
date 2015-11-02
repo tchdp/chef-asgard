3ef230c894c034fd11dddd7916c999073985a8d6
tchdp <perez.dp@gmail.com>
2015-11-01 19:51:52 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -33,6 +33,7 @@ default['asgard']['account_name'] = ""
 default['asgard']['public_resource_accounts'] = []
 
 default['asgard']['src_url'] = nil
+default['asgard']['war_url'] = 'https://github.com/Netflix/asgard/releases/download/1.5.1/asgard.war'
 
 default['asgard']['eureka']['active'] = false
 default['asgard']['eureka']['regions_to_servers'] = { 'US_EAST_1' => 'eureka.us-east-1.mydomain.com' }
